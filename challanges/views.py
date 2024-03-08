f4c98320b068d03dabeb7e9bc4d6369c4928917c
Azrul Amaline <azrul.amaline16@gmail.com>
2024-03-08 21:52:56 +0600

    created challange app

---
+++ challanges/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def january(request):
+    return HttpResponse("This is January.")
+
+def february(request):
+    return HttpResponse("This is February.")
+
+def march(request):
+    return HttpResponse("This is March.")
